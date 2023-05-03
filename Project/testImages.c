0ec5c6cac8c4527b009ebb414a17ce82da32bd17
Vision295 <78678991+Vision295@users.noreply.github.com>
2023-05-03 19:32:20 +0200

    doc for libpng

--- Project/testImages.c
+++ Project/testImages.c
@@ -1,20 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 FILE * ptr;
 
 int main()
 {
     ptr = fopen("test.png", "rb");
-    char tab[100] = {0};
-    fread(tab, 1, 100, ptr);
+    int tab[64] = {0};
+    fread(tab, 8, 64, ptr);
 
     for (int i = 0; i < sizeof(tab); i++)
     {
-        printf("%c", tab[i]);
+        printf("%d ", tab[i]);
     }
-    
     
     return 0;
 }
