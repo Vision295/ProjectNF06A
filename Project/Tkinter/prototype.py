9a73974a5ec9c652d4cb29c4353df94d95bbea91
Vision295 <78678991+Vision295@users.noreply.github.com>
2023-05-17 13:51:30 +0200

    fully fonctionnal windows explorer

--- Project/Tkinter/prototype.py
+++ Project/Tkinter/prototype.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import os
+from PIL import Image
+from PIL import ImageTk
 
 
 class Window(Tk):
@@ -13,9 +15,8 @@ class Window(Tk):
         self.geometry("800x500")
         self.lables = []
         self.folder_frame = Frame(self, bg='white')
-
-        # scroll system
         self.current_path = 'C:/users/theop/Documents/_Perso/'
+
         # scrollable text area where all the buttons to select an image are
         self.area_file_buttons = Text(self.folder_frame) 
         self.scroll_bar = Scrollbar(self, command=self.area_file_buttons.yview)
@@ -29,6 +30,7 @@ class Window(Tk):
         self.open()
     
     def empty_folder_frame(self):
+        """empty the scrollable frame of all files when a new folder is openned"""
         self.area_file_buttons.destroy()
         # scrollable text area where all the buttons to select an image are
         self.area_file_buttons = Text(self.folder_frame) 
@@ -41,10 +43,11 @@ class Window(Tk):
 
     def change_path(self, _path):
         """changes the self.current_path to self.curr_path +  _path if it is a directory and opens the image if it is a png"""
-        if os.path.isdir(os.path.abspath(self.current_path + _path)):
-            self.current_path += _path + '/'
-        if _path.endswith('png') or _path.endswith('.PNG'):
-            return
+        if os.path.isdir(os.path.abspath(self.current_path + _path + '/')):
+            self.current_path += _path
+            self.current_path += '/'
+        elif _path.endswith(".png") or _path.endswith(".PNG"):
+            self.current_path += _path
         self.open()
     
     def create_menu(self):
@@ -71,6 +74,7 @@ class Window(Tk):
             - if it is a file it doesn't do anything
             - if it is a folder in goes in the folder
         """
+        print(self.current_path)
         # this creates a list of all elements at the path given
         if os.path.isdir(os.path.abspath(self.current_path)):
             self.empty_folder_frame()
@@ -93,6 +97,30 @@ class Window(Tk):
                 self.area_file_buttons.window_create('end', window=self.lables[i])
                 self.area_file_buttons.insert('end','\n')
         
+        elif self.current_path.endswith('.png') or self.current_path.endswith('.PNG'):
+            # create a new window with the image depending on a fixed size
+            self.window_size = 500
+            self.image_window = Toplevel(self, height=self.window_size, width=self.window_size)
+
+            # loads the image in script
+            self.image = Image.open(os.path.abspath(self.current_path))
+            # reduce its size by a coefficient : self.coef in order to fit whithin the window
+            self.coef = int(max(self.image.width, self.image.height) // self.window_size)
+            # the coef can't be 0 so we change it to be 1 in order not to change the image
+            if self.coef <= 0 : self.coef = 1
+            # change the size by coefficient self.coef
+            self.image_size = (self.image.width // self.coef, self.image.height // self.coef)
+            self.image = self.image.resize(self.image_size)
+
+            # load the image on screen
+            self.image = ImageTk.PhotoImage(self.image)
+            self.label_image = Label(self.image_window, image=self.image)
+            self.label_image.pack()
+            self.image_window.protocol("WM_DELETE_WINDOW", self.image_back)
+            self.image_window.mainloop()
+
+
+
     def back(self):
         """function to go back in hierarchy"""
         # conversion str -> list to manipulate it easier
@@ -109,6 +137,11 @@ class Window(Tk):
         # add a slash for it to be readable
         self.current_path += '/'
         self.open()
+
+    def image_back(self):
+        """goes back in hierarchy for images when closing window"""
+        self.back()
+        self.image_window.destroy()
 
 
 mainScreen = Window()
